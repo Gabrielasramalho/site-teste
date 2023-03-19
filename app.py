f0a9b702a620974f42e484d634b6423a7b8f5c3d
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2023-03-18 22:22:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">inicial</a> 
+<a href="/sobre">Sobre</a>
+<a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def hell_world():
-  return "Olá, mundo! Esse é meu site. (Gabriela Soares)"
+  return menu + "Olá, mundo! Esse é meu site. (Gabriela Soares)"
   
   @app.route("/sobre")
 def sobre():
