f931f22210e35f2a3c4266506f4af8dd80420014
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2023-03-18 22:31:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
 from flask import Flask
+from tchan import ChannelScraper
+
 
 app = Flask(__name__)
 
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+    
 menu = """
-<a href="/">inicial</a> 
-<a href="/sobre">Sobre</a>
-<a href="/contato">Contato</a>
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
-def hell_world():
+def index():
   return menu + "Olá, mundo! Esse é meu site. (Gabriela Soares)"
-  
-  @app.route("/sobre")
-def sobre():
-  return "sobre"
-  
-  @app.route("/contato")
-def contato():
-  return "contato"
 
+@app.route("/sobre")
+def sobre():
+  return menu + "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return menu + "Aqui vai o conteúdo da página Contato"
+
+
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
